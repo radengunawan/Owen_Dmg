ca0bf6b580a85148712dfaa46893bc77520e7580
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-12 18:13:03 +0800

    Add project files.

---
+++ Owen_Dmg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Owen_Dmg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello !");
+        }
+    }
+}
